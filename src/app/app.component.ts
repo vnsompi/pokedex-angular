ce360d08dd505adde9be1a5db340405dd1425c3c
vainqueur <vnsompi@gmail.com>
2025-03-09 10:29:26 -0700

    changement du fichier inde.html et app.component.html puis ajout d'autre  fonctionalite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { computed } from '@angular/core';
 import{POKEMON_LIST} from './pokemon-list.fake';
+import { Pokemon } from './pokemon.models';
 
 
 
@@ -13,43 +14,47 @@ import{POKEMON_LIST} from './pokemon-list.fake';
 
 export class AppComponent {
   pokemon_list = signal(POKEMON_LIST);
-  title = 'Angular!';
+  
    // utilisation des signal 
-  name = signal("pikachou");
-  life = signal(21); // ici on a 21 vie
-    
-
 // je vais creer une fonction qui va retourner la taille du pokemon
 //  ici on doit utiliser computed pour calculer la taille cor le deriver 
 // appeler lesetat dervies 
 
-  size = computed(() => {
-    if(this.life() < 15){
-      return "petit";
+size(pokemon:Pokemon){
+  if(pokemon.life < 15){
+       return "petit";
   }
-  else if(this.life() > 25){;
+  if(pokemon.life > 25){
     return "grand";
-  } 
-  else{
-    return "moyen";
   }
-  });
-  // ajout d'un autre signale
-  imageSource = signal(
-    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
-    
-  );
+  return "moyen";
+}
 
-incrementLife(){
-  this.life.update((life)=> life + 1); // ici on incremente une vie
+
+incrementLife(pokemon:Pokemon){
+  pokemon.life += 1;
+  // this.life.update((life)=> life + 1); // ici on incremente une vie
   // vu que nous avons utiliser le signal la syntaxe change aussi
 }
-  decrementLife(){
-    this.life.update((life)=> life - 1);// }ici on decremente une vie
+  decrementLife(pokemon:Pokemon){
+    pokemon.life -= 1;
+    // this.life.update((life)=> life - 1);// }ici on decremente une vie
     
 }
 
 // je met un fonction qui va devoir afficher la taille de pokemon
+// size = computed(() => {
+  //   if(this.life() < 15){
+  //     return "petit";
+  // }
+  // else if(this.life() > 25){;
+  //   return "grand";
+  // } 
+  // else{
+  //   return "moyen";
+  // }
+  // });
+  // ajout d'un autre signale
 
 
 
